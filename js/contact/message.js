b5ec0018185e958916c9284818b2ecb31af32781
Lerry Samson <johnlerryramossamson@gmail.com>
2023-07-17 23:20:07 +0800

    Loading done

--- js/contact/message.js
+++ js/contact/message.js
@@ -1,33 +1,38 @@
 export const sendMessage = () => {
-    const form = document.querySelector('.contact .contact-form');
-    const name = form.querySelector('.name');
-    const email = form.querySelector('.email');
-    const message = form.querySelector('.message');
-    const formButton = form.querySelector('.send-btn');
+    try {
+        const form = document.querySelector('.contact .contact-form');
+        const name = form.querySelector('.name');
+        const email = form.querySelector('.email');
+        const message = form.querySelector('.message');
+        const formButton = form.querySelector('.send-btn');
 
-    const url = 'https://lerrysamsonbackend.cyclic.app/newmessage';
-    
-    formButton.addEventListener('click', async (ev) => {
-        ev.preventDefault();
+        const url = 'https://johnlerryramossamson.cyclic.app/newmessage';
+        
+        formButton.addEventListener('click', async (ev) => {
+            ev.preventDefault();
 
-        const data = { 
-            name: name.value, 
-            email: email.value, 
-            message: message.value 
-        };
+            const data = { 
+                name: name.value, 
+                email: email.value, 
+                message: message.value 
+            };
 
-        const result = await send(url, data);
-        console.log(result);
+            const response = await send(url, data);
+            console.log(response);
+            // make modal for sent status
 
-        // make modal for sent status
-    });
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+        });
+    } catch(error) {
+        console.log(error);
+    }
 }
 
 async function send(url, data) {
     const response = await fetch(url, {
         method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
         headers: { 'Content-type': 'application/json' },
         body: JSON.stringify(data)
     });
