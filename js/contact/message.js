f325965451a4ba1dfcfeb4cc3771e99e8505bc79
Lerry Samson <johnlerryramossamson@gmail.com>
2023-07-18 23:33:49 +0800

    Sending message with modal message status done

--- js/contact/message.js
+++ js/contact/message.js
@@ -1,14 +1,21 @@
+const form = document.querySelector('.contact .contact-form');
+const name = form.querySelector('.name');
+const email = form.querySelector('.email');
+const message = form.querySelector('.message');
+const formButton = form.querySelector('.send-btn');
+
 const modalSuccess = document.querySelector('.message-sent-success');
 const modalFailed = document.querySelector('.message-sent-failed');
+const errorMessage = document.querySelector('.error-message');
 
 export const sendMessage = () => {
-    const form = document.querySelector('.contact .contact-form');
-    const name = form.querySelector('.name');
-    const email = form.querySelector('.email');
-    const message = form.querySelector('.message');
-    const formButton = form.querySelector('.send-btn');
+    contactForm();
+    contactGmail();
+}
 
+function contactForm() {
     const url = 'https://lerrysamson.cyclic.app/newmessage';
+    // const url = '/newmessage';
     
     formButton.addEventListener('click', async (ev) => {
         ev.preventDefault();
@@ -20,8 +27,14 @@ export const sendMessage = () => {
         };
 
         const response = await send(url, data);
-        if (response?.ok) {
+        // console.log(response);
+        if (response?.success) {
             modalSuccess.showModal();
+
+            name.value = email.value = message.value ='';
+        } else {
+            errorMessage.textContent = response?.error || 'Something went wrong.';
+            modalFailed.showModal();
         }
     });
 }
@@ -39,4 +52,14 @@ async function send(url, data) {
         modalFailed.showModal();
         console.log(error);
     }
+}
+
+function contactGmail() {
+    const link = document.querySelector('.gmail-link');
+    link.addEventListener('click', () => {
+        const nameOfUser = name.value || '<Your Name Here>';
+        const messageOfUser = message.value || 'Your Message Here';
+        const alternative = `https://mail.google.com/mail/u/0/?fs=1&to=johnlerryramossamson@gmail.com&su=Message%20from%20${nameOfUser}%20via%20Your%20Portfolio%20Website&body=${messageOfUser}&tf=cm`;
+        link.href = alternative;
+    });
 }
