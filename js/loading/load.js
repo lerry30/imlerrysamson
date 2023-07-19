1db490129727e09a1d1c98e71d136b6c1d66c45b
Lerry Samson <johnlerryramossamson@gmail.com>
2023-07-19 12:19:20 +0800

    Fixed loading screen

--- js/loading/load.js
+++ js/loading/load.js
@@ -1,8 +1,9 @@
 let loadingBackground = document.querySelector('.loading-background');
 
 export function loadingScreen() {
-    const pageTop = Math.abs(sessionStorage.getItem('page-top')) - (innerHeight * 0.2) || 0; // subtract 20% since this loading container or background has 140vh of height to prevent displaying the behind from unexpected pushing downward of this loading container
-
+    const offset = innerHeight * 0.2; // 20%                                   20% * -1
+    const pageTop = Math.abs(sessionStorage.getItem('page-top')) - offset || (offset * -1); // subtract 20% since this loading container or background has 140vh of height to prevent displaying the behind from unexpected pushing downward of this loading container
+    
     loadingBackground.style.top = `${pageTop}px`;
     loadingBackground.style.display = 'flex';
     document.body.style.overflow = 'hidden';
--- js/loading/startup.js
+++ js/loading/startup.js
@@ -1,5 +1,6 @@
 const loadingBackground = document.querySelector('.loading-background');
-const pageTop = Math.abs(sessionStorage.getItem('page-top')) - (innerHeight * 0.2) || 0; // subtract 20% since this loading container or background has 140vh of height to prevent displaying the behind from unexpected pushing downward of this loading container
+const offset = innerHeight * 0.2; // 20%                                   20% * -1
+const pageTop = Math.abs(sessionStorage.getItem('page-top')) - offset || (offset * -1); // subtract 20% since this loading container or background has 140vh of height to prevent displaying the behind from unexpected pushing downward of this loading container
 
 loadingBackground.style.top = `${pageTop}px`;
 document.body.style.overflow = 'hidden';
